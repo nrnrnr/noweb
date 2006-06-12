a9dda105a8fcfe99f4c3ec60db85172639e616c0
Norman Ramsey <nr@cs.tufts.edu>
2006-06-12 22:05:36 +0000

    standard checkin preparing to export version 2.11b

--- src/elisp/noweb-mode.el
+++ src/elisp/noweb-mode.el
@@ -18,8 +18,8 @@
 ;; 
 ;; See bottom of this file for information on language-dependent highlighting
 ;;
-;; $Id: noweb-mode.el,v 1.15 2006/05/20 14:36:38 nr Exp nr $
-;; $Name: v2_11a $
+;; $Id: noweb-mode.el,v 1.16 2006/06/12 21:03:57 nr Exp nr $
+;; $Name: v2_11b $
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; THIS IS UNRELEASED CODE: IT IS MISSING FUNCTIONALITY AND IT NEEDS CLEANUP ;;
@@ -76,10 +76,10 @@
 ;;; Variables
 
 (defconst noweb-mode-RCS-Id
-  "$Id: noweb-mode.el,v 1.15 2006/05/20 14:36:38 nr Exp nr $")
+  "$Id: noweb-mode.el,v 1.16 2006/06/12 21:03:57 nr Exp nr $")
 
 (defconst noweb-mode-RCS-Name
-  "$Name: v2_11a $")
+  "$Name: v2_11b $")
 
 (defvar noweb-mode-prefix "\M-n"
   "*Prefix key to use for noweb mode commands.
